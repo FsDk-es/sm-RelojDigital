36c37c0cf59d1a5590dbd7e06689b40940df22c3
FsDk <cayuela@gmail.com>
2022-04-16 00:10:37 +0200

    se le da formato a la hora con 1 function

--- js/reloj.js
+++ js/reloj.js
@@ -1,9 +1,20 @@
 const mostrarReloj = ()=>{
     let fecha = new Date();
-    let hr = fecha.getHours();
-    let min = fecha.getMinutes();
-    let seg = fecha.getSeconds();
+    let hr = formatoHora(fecha.getHours());
+    let min = formatoHora(fecha.getMinutes());
+    let seg = formatoHora(fecha.getSeconds());
     document.getElementById('hora').innerHTML = `${hr}:${min}:${seg}`;
 }
 
+const formatoHora = (hora) => {
+    if (hora < 10) {
+        hora = `0  ${hora}`;
+        return hora;
+    }
+  //  else  {
+
+  //      return hora;
+  //  }
+}
 setInterval(mostrarReloj, 1000);
+
