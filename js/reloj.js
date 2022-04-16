54a43b2d80febbee41c125966a0856aecf4af2b8
FsDk <cayuela@gmail.com>
2022-04-16 14:10:31 +0200

    se soluciona el bug del if .. en sus brackets..

--- js/reloj.js
+++ js/reloj.js
@@ -1,20 +1,26 @@
-const mostrarReloj = ()=>{
-    let fecha = new Date();
-    let hr = formatoHora(fecha.getHours());
-    let min = formatoHora(fecha.getMinutes());
-    let seg = formatoHora(fecha.getSeconds());
-    document.getElementById('hora').innerHTML = `${hr}:${min}:${seg}`;
+const mostrarReloj = ()=> {
+  let fecha = new Date();
+  let hr = formatoHora(fecha.getHours());
+  let min = formatoHora(fecha.getMinutes());
+  let seg = formatoHora(fecha.getSeconds());
+  document.getElementById('hora').innerHTML = `${hr}:${min}:${seg}`;
+  const meses = ["Enero", "Febrero", "Marzo", "Abril",   "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+  const dias = ["Domingo", "Lunes",  "Martes", "Miercoles",  "Jueves", "Viernes", "Sábado"];
+  let diaSemana = dias[fecha.getDay()];
+  let dia = fecha.getDate();
+  let mes = meses[fecha.getMonth()];
+
+  let fechaTexto = `${diaSemana}, ${dia} ${mes}`;
+  document.getElementById("fecha").innerHTML = fechaTexto;
+
+
 }
 
-const formatoHora = (hora) => {
-    if (hora < 10) {
-        hora = `0  ${hora}`;
+const formatoHora = (hora)=>{
+  if(hora < 10) 
+        hora = `0${hora}`;
         return hora;
-    }
-  //  else  {
+  }
 
-  //      return hora;
-  //  }
-}
+
 setInterval(mostrarReloj, 1000);
-
